60eb7f80ccd8a9d5ef1e05de4fecb73c74266482
241B045 <241B045@juet.co.in>
2026-07-27 15:44:08 +0530

    L1

---
+++ GTN.java
@@ -0,0 +1,10 @@
+public class GTN {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+        
+        int greatest = Math.max(a, b);
+        
+        System.out.println("great no. " + greatest);
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # APL2
 
 This repo contains lab exercises
+## Lab Exercise 1 
+
+Name: Arunam Jain   
+
+Roll Number: 241b045   
+
+Email: 241b045@juetguna.in    
+
+<Solution code to part F> 
 
