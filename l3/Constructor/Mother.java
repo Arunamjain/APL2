1308debc853e83c4c865770af5aa834bbb53bc5e
241B045 <241B045@juet.co.in>
2026-08-10 15:10:29 +0530

    l3

---
+++ l3/Constructor/Mother.java
@@ -0,0 +1,8 @@
+class Mother{
+   Mother(int x){
+       System.out.println("Heloo");
+   }
+    void show(int x){
+        System.out.println("Mother");
+    }
+}
---
+++ l3/Method Overriding/Mother.java
@@ -0,0 +1,8 @@
+class Mother{
+    void show(){
+        System.out.println("Child");
+    }
+    void show(int x){
+        System.out.println("Hello");
+    }
+}
