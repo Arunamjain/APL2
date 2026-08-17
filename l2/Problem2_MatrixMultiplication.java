ca3a0c1d0e552edbb5ffe8d3a731e6b74d600f47
241B045 <241B045@juet.co.in>
2026-08-17 15:31:42 +0530

    new: finished lab 2 and added assignment repo

---
+++ l2/Problem2_MatrixMultiplication.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Problem2_MatrixMultiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter rows and cols for Matrix A: ");
+            int rA = sc.nextInt(); int cA = sc.nextInt();
+            int[][] A = new int[rA][cA];
+            System.out.println("Enter elements of Matrix A:");
+            for (int i = 0; i < rA; i++) {
+                for (int j = 0; j < cA; j++) {
+                    A[i][j] = sc.nextInt();
+                }
+            }
+
+            System.out.print("Enter rows and cols for Matrix B: ");
+            int rB = sc.nextInt(); int cB = sc.nextInt();
+            int[][] B = new int[rB][cB];
+            System.out.println("Enter elements of Matrix B:");
+            for (int i = 0; i < rB; i++) {
+                for (int j = 0; j < cB; j++) {
+                    B[i][j] = sc.nextInt();
+                }
+            }
+
+            int[][] result = Matrix.multiplyMatrices(A, B);
+            System.out.println("Resulting Matrix:");
+            for (int[] row : result) {
+                System.out.println(Arrays.toString(row));
+            }
+        } catch (MatrixMismatchException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            sc.close();
+        }
+    }
+}
